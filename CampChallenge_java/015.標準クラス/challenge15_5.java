72705cc50c308934ae98385cff3c1c4cb7029714
たけなわ.com <y.solanin@gmail.com>
2018-02-27 16:49:47 +0900

    Javaの課題、バイト数と文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/challenge15_5.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yuta
+ */
+public class challenge15_5 {
+    public static void main(String[] args){
+        String name = "わたなべゆうた";
+        System.out.println(name.length());
+        byte[] list = name.getBytes();
+        System.out.print(list.length);
+        
+//        
+//        for(byte b : name.getBytes()){
+//            System.out.println(b);
+//        }
+        
+        
+    }
+    
+}
